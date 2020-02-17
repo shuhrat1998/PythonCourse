74896909c11a3dc1351ea507f1dbfe670e41d32c
Shuhrat <Shuhrat@SHUHRAT-PC.local>
2020-02-17 15:46:26 +0300

    Update FinModel.py

---
+++ HW1/FinModel.py
@@ -0,0 +1,13 @@
+#   INTL 550 - HW1
+#   Name : Shukhrat Khuseynov
+#   ID   : 0070495
+
+class Portfolio():
+    def __init__(self):
+        pass
+class Stock():
+    pass
+class MutualFund():
+    pass
+class Bond():
+    pass
