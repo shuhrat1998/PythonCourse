e938f8c6364eed055b2625fae84b20e9e588673b
Shuhrat <Shuhrat@SHUHRAT-PC.local>
2020-02-17 19:47:24 +0300

    Update FinModel.py

--- HW1/FinModel.py
+++ HW1/FinModel.py
@@ -4,24 +4,36 @@
 
 class Portfolio():
     def __init__(self):
-        pass
+        self.cash = 0
+        self.stocks = []
+        self.mfunds = []
+        self.history = ["Transactions:\n"]
     def addCash(self, cash):
+        self.cash += cash
+        transaction = str(cash) + "$ of cash are added to the portfolio.\n"
+        print(transaction)
+        self.history.append(str(len(self.history)-1)+". "+transaction)
+        
+    def buyStock(self, shares, symbol):
         pass
-    def buyStock(self, shares, stock):
+    def buyMutualFund(self, shares, symbol):
         pass
-    def buyMutualFund(self, shares, mfund):
+    def sellMutualFund(self, symbol, shares):
         pass
-    def sellMutualFund(self, mfund, shares):
-        pass
-    def sellStock(self, stock, shares):
+    def sellStock(self, symbol, shares):
         pass
     def withdrawCash(self, cash):
         pass
     def history(self):
         pass
-class Stock():
+    def __str__(self):
+        return "%s" %self.firstname
+
+class FinInstrument(object):
     pass
-class MutualFund():
+class Stock(FinInstrument):
     pass
-class Bond():
+class MutualFund(FinInstrument):
+    pass
+class Bond(FinInstrument):
     pass
