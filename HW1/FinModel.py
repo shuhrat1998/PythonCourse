7257e6daf1495f42c8a9c3570da3cd23ec78a7b4
Shuhrat <Shuhrat@SHUHRAT-PC.local>
2020-02-17 17:26:43 +0300

    Update FinModel.py

--- HW1/FinModel.py
+++ HW1/FinModel.py
@@ -5,6 +5,20 @@
 class Portfolio():
     def __init__(self):
         pass
+    def addCash(self, cash):
+        pass
+    def buyStock(self, shares, stock):
+        pass
+    def buyMutualFund(self, shares, mfund):
+        pass
+    def sellMutualFund(self, mfund, shares):
+        pass
+    def sellStock(self, stock, shares):
+        pass
+    def withdrawCash(self, cash):
+        pass
+    def history(self):
+        pass
 class Stock():
     pass
 class MutualFund():
