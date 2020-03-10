3600591edfeedf89d1451614e8451501e61555dd
Shuhrat <Shuhrat@SHUHRAT-PC.local>
2020-03-10 23:37:45 +0300

    Update Run.py

--- HW2/Run.py
+++ HW2/Run.py
@@ -6,7 +6,8 @@ ID   : 0070495
 
 import numpy as np
 import pandas as pd
-import Function as fn
+import Rfunction as fn
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('tjdata.csv')
 
@@ -22,8 +23,44 @@ data_n.insert(1, 'const', n * [1])
 
 # running without normalizing 
 beta, sterror, confidence = fn.reg(data['lifexp'], data[['const', 'gdp', 'school', 'birth']])
+output = pd.DataFrame([beta, sterror, confidence[0], confidence[1]])
+output.columns = ['Bi', 'SEi', 'CI1', 'CI2']
+print("\n(Initial)\n", output)
 
 # running the normalized version
 beta_n, sterror_n, confidence_n = fn.reg(data_n['lifexp'], data_n[['const', 'gdp', 'school', 'birth']])
+output_n = pd.DataFrame([beta_n, sterror_n, confidence_n[0], confidence_n[1]])
+output_n.columns = ['Bi', 'SEi', 'CI1', 'CI2']
+print("\n(Normalized)\n", output_n)
+
+# plotting
+i = np.array([0,1,2,3])
+
+# plot for beta (initial version)
+plt.plot(i, beta, 'o')
+plt.vlines(ymin = confidence[0], ymax = confidence[1], x=i, linestyle = '-')
+plt.title("Beta estimates and confidence interval (initial).", loc = 'center')
+plt.xlabel("- i -")
+plt.xticks(np.arange(0, 4, 1.0))
+plt.ylabel("- Bi -")
+plt.show()
+
+# plot for beta (initial version - closer)
+plt.plot(i[1:], beta[1:], 'o')
+plt.vlines(ymin = confidence[0][1:], ymax = confidence[1][1:], x=i[1:], linestyle = '-')
+plt.title("Beta estimates and confidence interval (initial) - closer.", loc = 'center')
+plt.xlabel("- i -")
+plt.xticks(np.arange(0, 4, 1.0))
+plt.ylabel("- Bi -")
+plt.show()
+
+# plot for beta (using normalized version)
+plt.plot(i, beta_n, 'o')
+plt.vlines(ymin = confidence_n[0], ymax = confidence_n[1], x=i, linestyle = '-')
+plt.title("Beta estimates and confidence interval (normalized).", loc = 'center')
+plt.xlabel("- i -")
+plt.xticks(np.arange(0, 4, 1.0))
+plt.ylabel("- Bi -")
+plt.show()
 
 # The end.
